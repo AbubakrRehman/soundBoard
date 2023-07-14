cafd4b6bb94acd13ef76b307ef2dba4c63436153
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-07-14 17:02:49 +0530

    stopd all prevous runng audios before playng one.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const btn__container = document.getElementById("btn__container");
+const audioElements = document.querySelectorAll("audio");
 
 btn__container.addEventListener("click", (e) => {
     if (e.target.id) {
+
+        audioElements.forEach((audio) => {
+            audio.pause();
+            audio.currentTime = 0;
+        })
+
         const sound_id = e.target.id;
         const sound_element = document.getElementById(sound_id);
         sound_element.play();
